f797d2a9e09a2f29fb98ea2632f091034353d2a8
William A. Paula <williamazevedodepaula@hotmail.com>
2020-05-17 11:52:19 -0300

    melhoria na organiacao do codigo

--- entity/Tax.ts
+++ entity/Tax.ts
@@ -3,7 +3,9 @@ import { Product } from "./Product";
 export class Tax{
 
     static getProductTax(product:Partial<Product>):number{   
-        let aliquot = (product.manufacturing == 'imported') ? 0.15 : ((product.price > 100) ? 0.1 : 0);
+        let aliquot = (product.manufacturing == 'imported') ? 0.15 : 
+                      ((product.price > 100) ? 0.1 : 0);
+
         return +((product.price||0) * aliquot).toFixed(2);
     }
 }
