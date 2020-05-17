95e7e55633236bbd934b5dff2481849b56dbfbea
William A. Paula <williamazevedodepaula@hotmail.com>
2020-05-17 11:42:07 -0300

    implementacao passando em todos os testes de calculo de imposto

--- entity/Tax.ts
+++ entity/Tax.ts
@@ -2,7 +2,8 @@ import { Product } from "./Product";
 
 export class Tax{
 
-    static getProductTax(product:Partial<Product>):number{        
+    static getProductTax(product:Partial<Product>):number{      
+        if(product.manufacturing == 'imported')   return ((product.price||0) / 100.0)*15;
         return (product.price > 100) ? ((product.price||0) / 100.0)*10 : 0;
     }
 }
