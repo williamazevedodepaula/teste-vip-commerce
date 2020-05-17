f4156ef69bba6a2d9e3992443ea2fcc7d036fd46
William A. Paula <williamazevedodepaula@hotmail.com>
2020-05-17 11:35:39 -0300

    refatoracao na classe Tax para passar no segundo teste de impostos

--- entity/Tax.ts
+++ entity/Tax.ts
@@ -2,7 +2,7 @@ import { Product } from "./Product";
 
 export class Tax{
 
-    static getProductTax(product:Partial<Product>):number{
-        return ((product?.price||0) / 100.0)*10;
+    static getProductTax(product:Partial<Product>):number{        
+        return (product.price > 100) ? ((product.price||0) / 100.0)*10 : 0;
     }
 }
