9ee87fad3b7235b3c0456f199a78967153d035fe
William A. Paula <williamazevedodepaula@hotmail.com>
2020-05-19 12:08:52 -0300

    documentadas as classes de serviço

--- service/OrderService.ts
+++ service/OrderService.ts
@@ -2,25 +2,51 @@ import { Order } from "entity/Order";
 import * as moment from "moment";
 import { OrderItem } from "entity/OrderItem";
 
+/**
+ * Classe para execução de métodos puros de serviço relacionados a pedidos
+ */
 export class OrderService{
 
+    /**
+     * @param cpf cpf do cliente
+     * @return o cpf informado, formatado com máscara de CPF
+     */
     static formatCpf(cpf:string):string{
         return `${cpf.slice(0,3)}.${cpf.slice(3,6)}.${cpf.slice(6,9)}-${cpf.slice(9,11)}`;
     }
 
+    /**
+     * @param orderNumber um número de pedidos
+     * @return o número informado, formatado com pelo menos 4 dígitos. Exemplo: 0001
+     */
     static formatOrderNumber(orderNumber:number):string{
         let strSize = Math.max(4,String(orderNumber).length);
         return ("0000"+orderNumber).slice(-strSize);
     } 
 
+    /**
+     * Calculo o total pago por um item de um pedido
+     * @param item o item de pedido
+     * @return o total pago pelo item (quantidade multiplicada pelo preço)
+     */
     static calculateItemTotal(item:OrderItem):number{
         return item.amount * (item.product?.price||0);
     }
 
+    /**
+     * Calculo o total pago por um pedido
+     * @param order o pedido
+     * @return o total pago pelo pedido (soma dos totais dos itens)
+     */
     static calculateTotal(order:Order):number{
         return order.itens?.reduce((sum,item)=>(sum+this.calculateItemTotal(item)),0)||0;
     }
 
+    /**
+     * Formata um corpode de email referente ao pedido, para ser enviado
+     * @param order o pedido
+     * @return string contendo o codigo HTML do email a ser enviado
+     */
     static formatEmailBody(order:Order):string{   
         let itens = order.itens?.reduce((itemList,item:OrderItem)=>{
             return itemList+
--- service/TaxService.ts
+++ service/TaxService.ts
@@ -2,8 +2,19 @@ import { Product } from "../entity/Product";
 import { Order } from "../entity/Order";
 import { Client } from "../entity/Client";
 
+/**
+ * Classe para execução de métodos puros de serviço relacionados a cálculo de impostos
+ */
 export class TaxService{
 
+    /**
+     * Calculoa o valor de imposto pago por um produto:
+     * * Produto nacinal, preço > R$ 100.00: Valor * 10%
+     * * Produto nacinal, preço <= R$ 100.00: 0
+     * * Produto importadp: Valor * 15%
+     * @param product o produto a ter imposto calculado
+     * @return o valor do imposto pago referente ao produto
+     */
     static getProductTax(product:Partial<Product>):number{           
         let aliquot = (product.manufacturing == 'imported') ? 0.15 : 
                       ((product.price > 100) ? 0.1 : 0);
@@ -11,6 +22,12 @@ export class TaxService{
         return +((product.price||0) * aliquot).toFixed(2);
     }
 
+    /**
+     * Calculoa o valor total de impostos pago em um pedido.
+     * (somatório dos impostos dos produtos nos itens do pedido)
+     * @param order o pedido a ter imposto calculado
+     * @return o valor do imposto pago referente ao pedido
+     */
     static getOrderTax(order:Partial<Order>):number{   
         return order.itens?.reduce(
             (total,item)=>total+(
@@ -18,6 +35,13 @@ export class TaxService{
             ,0)||0;
     }
 
+    /**
+     * Calculoa o valor total de impostos pago por um cliente
+     * (somatório dos impostos dos pedidos).
+     * O calculo será feito sobre todos os pedidos carregados no relacionamento com o cliente.
+     * @param client o cliente a ter imposto calculado
+     * @return o valor do imposto pago pelo cliente
+     */
     static getClientTax(client:Partial<Client>):number{        
         return client.orders?.reduce(
             (total,order)=>total+TaxService.getOrderTax(order)
